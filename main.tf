750ba7ef708fde4e27b40099f196128ff8f1d01c
Jon Sapsay <jsapsay@outlook.com>
2026-06-03 14:57:25 -0700

    Add storage account

--- main.tf
+++ main.tf
@@ -16,3 +16,11 @@ resource "azurerm_resource_group" "learning" {
   name     = "rg-learning-dev-eastus-001"
   location = "East US"
 }
+
+resource "azurerm_storage_account" "learning" {
+  name                     = "stlearningdev001"
+  resource_group_name      = azurerm_resource_group.learning.name
+  location                 = azurerm_resource_group.learning.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
